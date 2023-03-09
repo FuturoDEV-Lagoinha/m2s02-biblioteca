8dc9281d47d801b50d57f5adc6c3cf6a72eef97a
kahsh-augustin <gabriel.augustin@edu.sc.senai.br>
2023-03-08 22:15:29 -0300

    Primeiro commit

---
+++ src/br/com/lagoinha/biblioteca/Main.java
@@ -0,0 +1,39 @@
+package br.com.lagoinha.biblioteca;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        /*
+        Objetos:
+        - Editora
+            - *Nome         String
+        - Autor
+            - *Nome          String
+        - Livro
+            - *Título        String
+            -  Subtítulo     String
+            - *Gênero        Genero
+            - *Idioma        String
+            - *Código ISBN   String
+            - *Autores       Lista de Autor
+            - *Editora       Editora
+            - Qtd exemplares Integer
+        - Gênero (Ficção/Romance)
+            - *Nome          String
+
+
+
+
+
+
+        À fazer:
+        - Controle de quantidades disponíveis (empréstimo/venda)
+
+        */
+
+
+    }
+
+}
